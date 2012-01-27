c960508602e0a6a938d9c5511e4ca1844bb68733
yvan <yvan.strahm@gmail.com>
2012-01-27 13:50:17 +0100

    now I can go through the fastq

---
+++ SplitMeBioPython.py
@@ -0,0 +1,22 @@
+from sys import argv
+#try:
+#    os.path.exists(argv[1])
+#except:
+#    print "could not find file"
+    
+from Bio import SeqIO
+handle = open(argv[1], "rU")
+i=1
+for record in SeqIO.parse(handle, "fastq") :
+    if i < 101:
+        #print dir(record)
+        #print "1 ",record.id
+        #print "2 ",record.seq
+        #print "3 ",record.letter_annotations.keys()
+        #print "4 ",record.letter_annotations['phred_quality']
+        #print record.format("fastq-illumina") ## qualities are wrong
+        print record.format("fastq").rstrip()
+        i +=1
+    else:
+        exit("FINI")
+handle.close()
