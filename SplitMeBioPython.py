db2239e6cf6eb7b0c17ccb38e90f7d015192a4df
yvan <yvan.strahm@gmail.com>
2012-02-22 16:07:40 +0100

    better

--- SplitMeBioPython.py
+++ SplitMeBioPython.py
@@ -7,17 +7,29 @@ for a in argv:
 
 source = open(argv[1],"rU")
 i=0
-for record in SeqIO.parse(d, "fastq"):
+for record in SeqIO.parse(source, "fastq"):
     i += 1
 
 print "I ", i
-x = math.ceil(i/10.0)
+x = math.ceil(i/1000.0)
 print "X " , x
+#x=1763637.0
 
 #write subset
 
-target = []
-#not finisehd
+i = 0
+j = 1
+target = argv[1]+str(j)
 d = open(target,"w+")
+source = open(argv[1],"rU")
+
 for record in SeqIO.parse(source, "fastq") :
-    d.write(record.format("fastq"))
+    if i <= x:
+         i += 1
+         d.write(record.format("fastq"))
+    else:
+        d.write(record.format("fastq"))
+        i = 0
+        j += 1
+        target = argv[1]+str(j)
+        d =open(target,"w+")
